1fcb4a6c5f2fa1d67fea2229e7445101ab81060a
zhangpeng <zhangpeng@weishangtech.com>
2019-08-05 19:46:10 +0800

    合成/聚合复用原则

---
+++ src/com/demo/chapter22/Main.java
@@ -0,0 +1,13 @@
+package com.demo.chapter22;
+
+/**
+ * @Copyright (C), 2019,蔚商科技
+ * @desc:
+ * @author: zhangpeng
+ * @date: 2019/8/5 7:01 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
